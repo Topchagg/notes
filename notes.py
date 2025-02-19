cca04b3499661f6ae1ff3f5c01ababd1a631e93d
Topchagg <zahar.meednov@gmail.com>
2025-02-19 09:49:27 +0100

    Added note class

---
+++ notes.py
@@ -0,0 +1,13 @@
+class BaseNote():
+    def __init__(self,title,text,id):
+        self.__id = id
+        self.__title = title
+        self.__text = text
+
+    def getInfo(self):
+        return f"ID: {self.__id}\nTitle: {self.__title}\nText: {self.__text}"
+        
+    def updateNote(self, **kwargs):
+        for key, value in kwargs.items():
+            if hasattr(self, f"_{self.__class__.__name__}__{key}"):
+                setattr(self, f"_{self.__class__.__name__}__{key}", value)                        
